3de332088e2703ca77043deb92720c4f7ead7e53
Rui Guilherme <rui.cunha@cwi.com.br>
2022-11-14 06:24:39 -0300

    removendo ideias antigas

--- routes/router.js
+++ routes/router.js
@@ -8,10 +8,8 @@ import active from './public/active.routes.js'
 // admin routes
 import administrative_contents from './admin/contents.routes.js'
 import administrative_users from './admin/users.routes.js'
-import administrative_uploaders from './admin/uploaders.routes.js'
 
 // user routes
-import user_history from './user/history.routes.js'
 import user_profile from './user/profile.routes.js'
 import user_votes from './user/votes.routes.js'
 import user_comments from './user/comments.routes.js'
@@ -19,7 +17,7 @@ import user_comments from './user/comments.routes.js'
 const routes = Router()
 
 routes.use('/public', login, signup, active)
-routes.use('/admin', administrative_contents, administrative_users, administrative_uploaders)
-routes.use('/user', user_history, user_profile, user_votes, user_comments)
+routes.use('/admin', administrative_contents, administrative_users)
+routes.use('/user', user_profile, user_votes, user_comments)
 
 export default routes
